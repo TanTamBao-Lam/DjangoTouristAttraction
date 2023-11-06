84880dbac3566b31b223e189ea6e836b7a3cd0a9
Bao Lam <90415233+TanTamBao-Lam@users.noreply.github.com>
2023-11-06 19:06:57 -0400

    Adjust starter

--- TouristAttraction/TouristAttraction/urls.py
+++ TouristAttraction/TouristAttraction/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # home path.
+    path('', views.home, name="home"),
+    
+    # details page path.
+    path('details/<statename>', views.details, name="details")
 ]
