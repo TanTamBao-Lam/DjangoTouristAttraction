aa52b3e0062ef34a5a788ec9d2c9edbe96d87cad
Bao Lam <90415233+TanTamBao-Lam@users.noreply.github.com>
2023-11-06 19:10:01 -0400

    Add tourist_attractions app and bootstraps into Installed_apps in settings.py

--- TouristAttraction/TouristAttraction/settings.py
+++ TouristAttraction/TouristAttraction/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tourist_attractions',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
