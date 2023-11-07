927f62952b3ae6f5acc943c82fdb688e5cecf2f1
Bao Lam <90415233+TanTamBao-Lam@users.noreply.github.com>
2023-11-07 16:15:42 -0400

    Home page

--- TouristAttraction/TouristAttraction/settings.py
+++ TouristAttraction/TouristAttraction/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tourist_attractions',
+    'tourist_attractions.apps.TouristAttractionsConfig',
     'bootstrap5'
 ]
 
--- TouristAttraction/TouristAttraction/urls.py
+++ TouristAttraction/TouristAttraction/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from tourist_attractions import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
